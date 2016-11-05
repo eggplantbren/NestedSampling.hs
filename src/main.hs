5badc4de5242c188afdf6555cb37ab284935e38d
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-11-05 19:26:44 +1300

    Nice printing

--- src/main.hs
+++ src/main.hs
@@ -4,10 +4,20 @@ import NestedSampling.SpikeSlab
 main = do
     -- Set the seed
     -- setSeed 123
-    putStrLn "# NestedSampling.hs"
+
+    -- Print some messages
     putStrLn "# Generating 10 particles from the prior\
                        \ and printing their log likelihoods"
-    let actions = [fromPrior | i <- [0..9]]
+
+    -- List of IO actions which generate particles from the prior
+    let actions = [fromPrior | i <- [0..9]] :: [IO [Double]]
+
+    -- Execute the actions
     particles <- sequence actions
-    print $ map logLikelihood particles
+
+    -- Get the log likelihoods and turn them into strings
+    let s = (map show $ map logLikelihood particles) :: [String]
+
+    -- Print them
+    putStrLn $ foldl (++) "" $ map (++ "\n") s
 
