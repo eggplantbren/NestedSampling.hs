c8154c1db7c6b3ecd093a96dd3632e553de81eab
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-11-05 19:06:53 +1300

    Some comments and formatting changes

--- src/NestedSampling/SpikeSlab.hs
+++ src/NestedSampling/SpikeSlab.hs
@@ -7,35 +7,38 @@ import NestedSampling.Utils
 -- Log likelihood function
 logLikelihood :: [Double] -> Double
 logLikelihood params = logsumexp [100*logl1, logl2]
-            where logl1 = -0.5 * (sum $ map (\x -> ((x-shift)/u)**2 + z) params)
-                  logl2 = -0.5 * (sum $ map (\x -> (x/v)**2 + z) params)
-                  z = log (2*pi) :: Double
-                  u = 0.01 :: Double
-                  v = 0.1 :: Double
-                  shift = 0.03 :: Double
+    where logl1 = -0.5 * (sum $ map (\x -> ((x-shift)/u)**2 + z) params)
+          logl2 = -0.5 * (sum $ map (\x -> (x/v)**2 + z) params)
+          z = log (2*pi) :: Double
+          u = 0.01 :: Double
+          v = 0.1 :: Double
+          shift = 0.03 :: Double
 
--- Lives in IO and generates parameters from the prior
+-- fromPrior is an IO action that returns a list of doubles
+-- representing a point in the parameter space
 fromPrior :: IO [Double]
 fromPrior = do
-                x <- rand 20
-                let y = map (\a -> a - 0.5) x
-                return y
+    x <- rand 20
+    let y = map (\a -> a - 0.5) x
+    return y
 
--- Perturb
-perturb :: [Double] -> IO [Double]
+-- Perturb takes a list of doubles as input
+-- and returns an IO action that returns the
+-- perturbed particle and the logH value.
+perturb :: [Double] -> IO ([Double], Double)
 perturb params = do
-                    -- Choose a parameter to perturb
-                    k <- randInt $ length params
+    -- Choose a parameter to perturb
+    k <- randInt $ length params
 
-                    -- Draw from randh
-                    rh <- randh 1
+    -- Draw from randh
+    rh <- randh 1
 
-                    -- Construct proposal vector
-                    let proposal = [if i==k then
-                                        (perturbSingle (params !! i) (rh !! 0))
-                                        else (params !! i) |
-                                                i <- [0..(length params - 1)]]
-                    return proposal
+    -- Construct proposal vector
+    let proposal = [if i==k then
+                        (perturbSingle (params !! i) (rh !! 0))
+                        else (params !! i) |
+                                i <- [0..(length params - 1)]]
+    return (proposal, 0.0)
 
 -- Perturb a single double using the provided randh and
 -- wrap back into [-0.5, 0.5]
