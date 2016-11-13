943118bb6b5cbe80f5a3a2e3f969045ddd8e0eae
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-11-14 12:47:44 +1300

    More sensible run length

--- src/Main.hs
+++ src/Main.hs
@@ -6,10 +6,10 @@ import System.Random.MWC
 main :: IO ()
 main = withSystemRandom . asGenIO $ \gen -> do
     -- Create a sampler
-    sampler <- generateSampler 10 1000 gen
+    sampler <- generateSampler 100 1000 gen
 
-    -- Do 100 NS iterations
-    nestedSamplingIterations 2000 sampler gen
+    -- Do 10000 NS iterations
+    nestedSamplingIterations 10000 sampler gen
 
     return ()
 
