c70d6fa5ae724208b928e82ef71519b3ef97a9d5
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-11-18 16:30:08 +1300

    comments

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,10 @@ import System.Random.MWC
 
 main :: IO ()
 main = withSystemRandom . asGenIO $ \gen -> do
-    -- Create a sampler
+    -- Create a sampler with 100 particles, and 1000 MCMC steps per NS iteration
     sampler <- generateSampler 100 1000 gen
 
-    -- Do 10000 NS iterations
+    -- Do 10000 NS iterations (this'll go to a depth of 100 nats)
     nestedSamplingIterations 10000 sampler gen
 
     return ()
