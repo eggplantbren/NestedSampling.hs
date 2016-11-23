ddc75b2454d6a81a339d81eb287fbfacd17efa87
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-11-23 17:00:12 +1300

    Need more iterations...

--- src/Main.hs
+++ src/Main.hs
@@ -8,8 +8,8 @@ main = withSystemRandom . asGenIO $ \gen -> do
     -- Create a sampler with 100 particles, and 1000 MCMC steps per NS iteration
     sampler <- generateSampler 1000 1000 gen
 
-    -- Do 10000 NS iterations (this'll go to a depth of 100 nats)
-    nestedSamplingIterations 10000 sampler gen
+    -- Do 100000 NS iterations (this'll go to a depth of 100 nats)
+    nestedSamplingIterations 100000 sampler gen
 
     return ()
 
