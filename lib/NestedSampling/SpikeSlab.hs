4b1dfcc961f1630cdbd55101abc1e96be6a585e7
Jared Tobin <jared@jtobin.ca>
2016-12-05 10:14:17 +1300

    Plunk randh in Utils.

--- lib/NestedSampling/SpikeSlab.hs
+++ lib/NestedSampling/SpikeSlab.hs
@@ -5,7 +5,6 @@ import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
 import NestedSampling.Utils
 import System.Random.MWC as MWC (Gen, uniform, uniformR)
-import System.Random.MWC.Distributions as MWC (standard)
 
 -- The SpikeSlab model --
 
@@ -48,16 +47,4 @@ perturb particle gen = do
   where
     perturbSingle :: Double -> Double -> Double
     perturbSingle x rh = (`wrap` (-0.5, 0.5)) $ x + rh
-
--- My favourite heavy tailed distribution
-randh :: Gen RealWorld -> IO Double
-randh gen = do
-    a <- MWC.standard gen
-    b <- MWC.uniform gen
-    n <- MWC.standard gen
-    return $! transform a b n
-  where
-    transform a b n =
-      let t = a/sqrt (- (log b))
-      in  10.0**(1.5 - 3.0*(abs t))*n
 
