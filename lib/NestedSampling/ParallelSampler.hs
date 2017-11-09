2f3ad53d43296e0c1af76f237a8da9732b988d58
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-11-09 19:09:58 +1300

    Making a smart constructor

--- lib/NestedSampling/ParallelSampler.hs
+++ lib/NestedSampling/ParallelSampler.hs
@@ -1,15 +1,31 @@
-module NestedSampling.ParallelSampler where
+module NestedSampling.ParallelSampler (makeParallelSampler) where
 
 -- Imports
 import Control.Monad.Primitive
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Maybe
 import qualified Data.Vector as V
+import Data.Word
 import NestedSampling.Sampler
-import System.Random.MWC
+import NestedSampling.Utils
+import System.Random.MWC as MWC
 
+-- A type representing parallel sampler (contains many regular samplers)
 data ParallelSampler a = ParallelSampler
                          {
-                             psNumThreads :: !Int,
+                             psNumThreads :: !Word32,
                              psSamplers   :: !(V.Vector (Sampler a)),
                              psGenerators :: !(V.Vector (Gen RealWorld))
                          }
+
+-- A smart constructor
+makeParallelSampler :: Word32 -> Word32 -> MaybeT IO (ParallelSampler a)
+makeParallelSampler numThreads firstSeed
+  | numThreads <= 1 = hoistMaybe Nothing
+  | otherwise       = do
+        let seeds = V.fromList $ map (+firstSeed) [0..(numThreads-1)]
+        generators <- lift $ V.mapM (MWC.initialize . V.singleton) seeds
+        let sampler = ParallelSampler
+                        numThreads (V.fromList []) generators
+        return $! sampler
 
